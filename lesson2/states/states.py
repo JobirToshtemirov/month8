115dce29c08f22ac33a7a9347b6e80cbe24bc473
Jobir <toshtemirovjobir6@gmail.com>
2024-11-24 18:39:56 +0500

    asfaf

--- lesson2/states/states.py
+++ lesson2/states/states.py
@@ -4,3 +4,8 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class UserStates(StatesGroup):
     get_message = State()
 
+
+class RegisterState(StatesGroup):
+    language = State()
+    full_name = State()
+    phone_number = State()
